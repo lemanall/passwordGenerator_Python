6b3d8a874807586e1f57a2eeb0bbd0dcbf8ea21f
lemanall <83183619+lemanall@users.noreply.github.com>
2024-11-10 17:15:18 +0400

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,47 +1,4 @@
-# fruits = ['apple', 'orange', 'pear']
-# for fruit in fruits:
-#     print(fruit)
-#     print(fruit + ' Pie')
 
-# students_height =  input("Enter students height: ").split(',')
-# sum = 0
-#
-# print(students_height)
-# for n in range(0, len(students_height)):
-#     sum += int(students_height[n])
-#
-# average = sum / len(students_height)
-# print(round(average, 0))
-
-
-# student_scores = input("Enter students scores: ").split(',')
-# big = int(student_scores[0])
-#
-# for n in range(0, len(student_scores)):
-#    student_scores[n] = int(student_scores[n])
-#    if student_scores[n] > big:
-#        big = student_scores[n]
-# print(big)
-# print(student_scores)
-
-#
-# sum = 0
-#
-# for n in range(1,101):
-#     if n%2 == 0:
-#         sum+= n
-#
-# print(sum)
-
-# for n in range(1,101):
-#     if n % 3 == 0 and n % 5 !=0:
-#         print('Fizz')
-#     elif n % 3 !=0 and n % 5 ==0:
-#         print('Buzz')
-#     elif n% 3 == 0 and n% 5 == 0:
-#         print('FizzBuzz')
-#     else:
-#         print(n)
 import random
 
 
